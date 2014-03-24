bbc992ba6ac7809b263bbc43fa7f408c6314e884
Jonathan Druart <jonathan.druart@biblibre.com>
2014-03-24 20:37:19 +0100

    Add welcome page

--- lib/bzsplitter.pm
+++ lib/bzsplitter.pm
@@ -1,7 +1,15 @@
-package bzsplitter;
+package BZSplitter;
+
+use Modern::Perl;
 use Dancer ':syntax';
 
 our $VERSION = '0.1';
+
+our $config = {
+    bugzilla => {
+        base_url => 'http://bugs.koha-community.org/bugzilla3/show_bug.cgi?id=',
+    }
+};
 
 get '/' => sub {
     template 'index';
