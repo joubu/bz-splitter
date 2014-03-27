1e67977cd3635a156402e90be5f6e60a052692b6
Jonathan Druart <jonathan.druart@biblibre.com>
2014-03-27 19:14:42 +0100

    Browse by authors - Add all the same

--- lib/bzsplitter.pm
+++ lib/bzsplitter.pm
@@ -28,12 +28,29 @@ get '/files' => sub {
     };
 };
 
+get '/authors' => sub {
+    my $authors = get_authors();
+    template 'authors' => {
+        action  => 'list',
+        authors => $authors,
+    };
+};
+
 ajax '/bugs/file/' => sub {
     my $filepath = params->{filepath};
     my $bugs = get_bugs_by_filepath( { filepath => $filepath } );
     {
         bugs     => $bugs,
         base_url => $config->{bugzilla}{base_url},
+    };
+};
+
+ajax '/bugs/authors/:author_name' => sub {
+    my $author_name = params->{author_name};
+    my $bugs = get_bugs_by_authorname( { author_name => $author_name } );
+    {
+        author_name => $author_name,
+        bugs        => $bugs,
     };
 };
 
@@ -55,8 +72,23 @@ ajax '/patches/bug/:bug_id/file/' => sub {
     };
 };
 
-
-
+ajax '/patches/bug/:bug_id/author/:author_name' => sub {
+    my $bug_id      = param('bug_id');
+    my $author_name = param('author_name');
+    my $patches     = get_patches(
+        {
+            bug_id      => $bug_id,
+            author_name => $author_name,
+        }
+    );
+    for my $patch (@$patches) {
+        $patch->{diff} = encode_entities( $patch->{diff} );
+    }
+    {
+        author_name => $author_name,
+        patches     => $patches,
+    };
+};
 
 sub get_files {
     my ($filters) = @_;
@@ -69,6 +101,24 @@ sub get_files {
             GROUP BY filepath
             ORDER BY filepath
         |,
+        { Slice => {} }
+    );
+}
+
+sub get_authors {
+    my ($filters) = @_;
+    return database->selectall_arrayref(
+        q|
+            SELECT  distinct(author_name),
+                    SUM(num_lines_added) AS num_lines_added,
+                    SUM(num_lines_deleted) AS num_lines_deleted,
+                    COUNT(diff) AS num_patches,
+                    COUNT(DISTINCT(bug_id)) AS num_bugs
+            FROM diff
+            GROUP BY author_name
+            ORDER BY author_name
+        |,
+
         { Slice => {} }
     );
 }
@@ -93,22 +143,45 @@ sub get_bugs_by_filepath {
     );
 }
 
+sub get_bugs_by_authorname {
+    my ($params) = @_;
+    my $author_name = $params->{author_name};
+    return database->selectall_arrayref(
+        q|
+            SELECT  DISTINCT(bug_id),
+                    bug_title,
+                    bug_status,
+                    SUM(num_lines_added) AS num_lines_added,
+                    SUM(num_lines_deleted) AS num_lines_deleted
+            FROM diff
+            WHERE author_name = ?
+            GROUP BY bug_id
+            ORDER BY bug_id
+        |,
+        { Slice => {} },
+        $author_name
+    );
+}
+
 sub get_patches {
     my ($params)    = @_;
     my $bug_id      = $params->{bug_id};
     my $filepath    = $params->{filepath};
+    my $author_name = $params->{author_name};
     return database->selectall_arrayref(
         q|
             SELECT date, diff, attachment_description
             FROM diff
             WHERE bug_id = ? |
               . ( $filepath    ? q| AND filepath = ? |    : '' )
+              . ( $author_name ? q| AND author_name = ? | : '' )
               . q| ORDER BY date
         |,
         { Slice => {} },
         (
             $bug_id,
             ( $filepath    ? $filepath    : () ),
+            ( $author_name ? $author_name : () )
         )
     );
 }
--- public/javascripts/bz-splitter.js
+++ public/javascripts/bz-splitter.js
@@ -12,6 +12,20 @@ function build_buglist_node_for_filepath(bugs, filepath) {
     return bug_list;
 }
 
+function build_buglist_node_for_author(bugs, author_name) {
+    var bug_list = $('<div class="bug-list"></div>');
+    $(bugs).each(function(){
+      var new_bug_node = $('<div class="patch" data-bug_id=' + this['bug_id'] + ' data-author_name="' + author_name + '" ></div>');
+      var title = $('<h4>Bug ' + this['bug_id'] + ':' + this['bug_title'] + ' ['+ this['bug_status'] + ']</h4>');
+      $(title).appendTo(new_bug_node);
+      $('<div></div>').appendTo(new_bug_node);
+
+      $('<span class="infos"><span class="num_lines_added"><span class="ui-icon ui-icon-circle-plus"></span>'+this['num_lines_added']+'</span><span class="num_lines_deleted"><span class="ui-icon ui-icon-circle-minus"></span>'+this['num_lines_deleted'] + '</span>').appendTo(new_bug_node);
+      $(new_bug_node).appendTo(bug_list);
+    });
+    return bug_list;
+}
+
 function build_patchlist_node(patches, filepath) {
     var patch_list = $('<div class="patch-list"></div>');
     $(patches).each(function(){
@@ -36,11 +50,20 @@ function add_accordion_to_bugs ( buglist_node ) {
         var parent_node = ui.newPanel.parent();
         var bug_id = parent_node.data('bug_id');
         var filepath = parent_node.data('filepath');
-        var url = '/patches/bug/'+bug_id+'/file/?filepath=' + filepath;
-        $.getJSON( url, {format: 'json' }).done(function(data){
-          var html = build_patchlist_node( data.patches );
-          ui.newPanel.html( html );
-        });
+        var author_name = parent_node.data('author_name');
+        if ( filepath ) {
+          var url = '/patches/bug/'+bug_id+'/file/?filepath=' + filepath;
+          $.getJSON( url, {format: 'json' }).done(function(data){
+            var html = build_patchlist_node( data.patches );
+            ui.newPanel.html( html );
+          });
+        } else {
+          var url = '/patches/bug/'+bug_id+'/author/' + author_name;
+          $.getJSON( url, {format: 'json' }).done(function(data){
+            var html = build_patchlist_node( data.patches );
+            ui.newPanel.html( html );
+          });
+        }
       }
     },
     activate: function(event, ui) {
@@ -50,6 +73,7 @@ function add_accordion_to_bugs ( buglist_node ) {
     }
   });
 }
+
 
 function add_accordion_to_filepaths( filepathlist_node ) {
   $( filepathlist_node ).accordion({
@@ -66,6 +90,29 @@ function add_accordion_to_filepaths( filepathlist_node ) {
         var url = '/bugs/file/?filepath=' + filepath;
         $.getJSON( url, {format: 'json' }).done(function(data){
           var new_buglist_node = build_buglist_node_for_filepath( data.bugs, filepath );
+          ui.newPanel.html( new_buglist_node );
+          add_accordion_to_bugs( new_buglist_node );
+        });
+      }
+    }
+  });
+}
+
+function add_accordion_to_authors( authorlist_node ) {
+  $( authorlist_node ).accordion({
+    collapsible: true,
+    clearStyle: true,
+    autoHeight: true,
+    heightStyle: 'content',
+    active: false,
+    icons: { "header": "ui-icon-plus", "activeHeader": "ui-icon-minus" },
+    header: "h3",
+    beforeActivate: function(event, ui) {
+      if ( ui.newPanel.html() == '' ) {
+        var author_name = ui.newPanel.parent().data('author_name');
+        var url = '/bugs/authors/' + author_name;
+        $.getJSON( url, {format: 'json' }).done(function(data){
+          var new_buglist_node = build_buglist_node_for_author( data.bugs, author_name );
           ui.newPanel.html( new_buglist_node );
           add_accordion_to_bugs( new_buglist_node );
         });
@@ -93,6 +140,24 @@ function reorder_filepaths_by_alpha(){
     };
     $('#filepath-list > div.filepath').sort(sortNumChanges).appendTo('#filepath-list');
 }
+function reorder_authors_by_add(){
+    function sortNumChanges(a,b){
+        return $(a).data("add") >$(b).data("add") ? -1 : 1;
+    };
+    $('#author-list > div.author').sort(sortNumChanges).appendTo('#author-list');
+}
+function reorder_authors_by_del(){
+    function sortNumChanges(a,b){
+        return $(a).data("del") >$(b).data("del") ? -1 : 1;
+    };
+    $('#author-list > div.author').sort(sortNumChanges).appendTo('#author-list');
+}
+function reorder_authors_by_alpha(){
+    function sortNumChanges(a,b){
+        return $(a).data("author_name") >$(b).data("author_name") ? 1 : -1;
+    };
+    $('#author-list > div.author').sort(sortNumChanges).appendTo('#author-list');
+}
 
 var delay = (function(){
   var timer = 0;
@@ -112,6 +177,15 @@ $(document).ready(function(){
   $(".filepath-sort-by > .sort-by-alpha").click(function(){
     reorder_filepaths_by_alpha();
   });
+  $(".author-sort-by > .sort-by-add").click(function(){
+    reorder_authors_by_add();
+  });
+  $(".author-sort-by > .sort-by-del").click(function(){
+    reorder_authors_by_del();
+  });
+  $(".author-sort-by > .sort-by-alpha").click(function(){
+    reorder_authors_by_alpha();
+  });
 
   $("#filepath-filter").on('keyup', function(){
     var pattern = $(this).val();
@@ -126,8 +200,25 @@ $(document).ready(function(){
       }).show();
     }, 1000);
   });
+  $("#author-filter").on('keyup', function(){
+    var pattern = $(this).val();
+    delay(function(){
+      var author_nodes = $("#author-list div.author");
+      $(author_nodes).hide();
+      $(author_nodes).filter(function( index ) {
+        if ( $(this).attr('data-author_name').match(".*"+pattern+".*") ) {
+          return 1
+        }
+        return 0
+      }).show();
+    }, 1000);
+  });
+
   $(function() {
     add_accordion_to_filepaths( $("#filepath-list") );
+  });
+  $(function() {
+    add_accordion_to_authors( $("#author-list") );
   });
 });
 
